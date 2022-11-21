f6b2cbc3139d3054484fd573685ab97bf78f7a24
Hasan Demirtaş <utsukushihito@outlook.com>
2022-11-22 00:55:10 +0300

    add more options.

--- plugin-gradle/src/main/kotlin/io/github/portlek/smol/func/GradleKts.kt
+++ plugin-gradle/src/main/kotlin/io/github/portlek/smol/func/GradleKts.kt
@@ -7,7 +7,7 @@ import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.provider.Provider
-
+import org.gradle.internal.Cast.uncheckedCast
 fun DependencyHandler.smol(
     dependencyNotation: Provider<*>,
     dependencyOptions: Action<in ExternalModuleDependency>
@@ -22,8 +22,32 @@ fun DependencyHandler.smolApi(
   addProvider(SMOL_API_CONFIGURATION_NAME, dependencyNotation, dependencyOptions)
 }
 
-fun DependencyHandler.smol(dependencyNotation: Any): Dependency? =
-    add(SMOL_CONFIGURATION_NAME, dependencyNotation)
+fun DependencyHandler.smol(
+    dependencyNotation: Any
+) = add(SMOL_CONFIGURATION_NAME, dependencyNotation)
 
-fun DependencyHandler.smolApi(dependencyNotation: Any): Dependency? =
-    add(SMOL_API_CONFIGURATION_NAME, dependencyNotation)
+fun DependencyHandler.smolApi(
+    dependencyNotation: Any
+) = add(SMOL_API_CONFIGURATION_NAME, dependencyNotation)
+
+fun DependencyHandler.smol(
+    dependencyNotation: String,
+    configure: Action<ExternalModuleDependency>?
+) = withOptions(SMOL_CONFIGURATION_NAME, dependencyNotation, configure)
+
+fun DependencyHandler.smolApi(
+    dependencyNotation: String,
+    configure: Action<ExternalModuleDependency>?
+) = withOptions(SMOL_API_CONFIGURATION_NAME, dependencyNotation, configure)
+
+private fun DependencyHandler.withOptions(
+    configuration: String,
+    dependencyNotation: String,
+    dependencyConfiguration: Action<ExternalModuleDependency>?
+): ExternalModuleDependency? = run {
+    uncheckedCast<ExternalModuleDependency>(create(dependencyNotation)).also { dependency ->
+        if (dependency == null) return@run null
+        dependencyConfiguration?.execute(dependency)
+        add(configuration, dependency)
+    }
+}
